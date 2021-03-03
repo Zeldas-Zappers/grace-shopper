8386a66244b997c79e0bad0912a3b894ca21c89b
Jae Chung <jaepublic@gmail.com>
2021-03-03 16:30:37 -0600

    refactor: automatic prettier formatting

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,37 +4,37 @@ const db = require('../db')
 const Order = db.define('order', {
   items: {
     type: Sequelize.ARRAY(Sequelize.STRING),
-    defaultValue: []
+    defaultValue: [],
   },
 
   total: {
-    type: Sequelize.DECIMAL(10, 2)
+    type: Sequelize.DECIMAL(10, 2),
   },
 
   shippingAddress: {
     type: Sequelize.TEXT,
     validate: {
-      notEmpty: true
-    }
+      notEmpty: true,
+    },
   },
 
   orderStatus: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
 
   promoCode: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
   },
 
   giftWrapping: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false
+    defaultValue: false,
   },
 
   giftStatus: {
     type: Sequelize.BOOLEAN,
-    defaultValue: false
-  }
+    defaultValue: false,
+  },
 })
 
 module.exports = Order
