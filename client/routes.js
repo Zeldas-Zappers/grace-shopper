62cfc68fc99dc9e1b5922c4a6cf41c4b81edb2e2
Jae Chung <jaepublic@gmail.com>
2021-03-07 10:26:28 -0600

    feat: get welcome message to render upon login and persist across pages

--- client/routes.js
+++ client/routes.js
@@ -22,25 +22,27 @@ class Routes extends Component {
     const {isLoggedIn} = this.props
 
     return (
-      <Switch>
-        {/* Routes placed here are available to all visitors */}
+      // <Switch>
+      // {/* Routes placed here are available to all visitors */}
+      <div>
+        {isLoggedIn && (
+          // <Switch>
+          // {/* Routes placed here are only available after logging in */}
+          <Route path="/" component={UserHome} />
+          // {/* </Switch> */}
+        )}
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
         <Route exact path="/products" component={AllProducts} />
         <Route exact path="/products/:productId" component={SingleProduct} />
         <Route exact path="/cart" component={Cart} />
-        <Route path="/" component={Home} />
+        <Route exact path="/" component={Home} />
         <Route exact path="/admin" component={Admin} />
 
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
-      </Switch>
+        {/* // </Switch> */}
+      </div>
     )
   }
 }
@@ -48,19 +50,19 @@ class Routes extends Component {
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
   }
 }
 
-const mapDispatch = dispatch => {
+const mapDispatch = (dispatch) => {
   return {
     loadInitialData() {
       dispatch(me())
-    }
+    },
   }
 }
 
@@ -73,5 +75,5 @@ export default withRouter(connect(mapState, mapDispatch)(Routes))
  */
 Routes.propTypes = {
   loadInitialData: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
+  isLoggedIn: PropTypes.bool.isRequired,
 }
